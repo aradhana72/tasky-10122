d34d196d645b6060b49cca0574382fa5c5b50ef3
aradhana72 <aradhanakund@gmail.com>
2022-02-20 18:20:12 +0530

    Card

---
+++ index.js
@@ -0,0 +1,37 @@
+const taskContainer = document.querySelector(".task_container");
+let globalTaskData=[];
+
+//TEMPLATE LITERAL - ``
+
+//assume that we have the modal data in a variable -> taskData
+//taskData -> object -> key value pairs
+/*{
+  id, image, title, type, description
+}*/
+//${}-> to put in dynamic nd changing js value in html
+//taskData.key == value
+const generateHTML = (taskData) => {
+  return ` <div id=${taskData.id} class="col-4">
+    <div class="card">
+<div class="card-header d-flex justify-content-end gap-2">
+<button type="button" id=${taskData.id} class="btn btn-outline-success">
+ <i class="fa fa-pencil" aria-hidden="true"></i>
+</button>
+<button type="button" id=${taskData.id} class="btn btn-outline-danger">
+ <i class="fa fa-trash" aria-hidden="true"></i>
+</button>
+</div>
+<div class="card-body">
+<img
+src=${taskData.image}
+alt="image"
+class="card-img-top"
+/>
+
+<h5 class="card-title mt-4 fw-bolder" id="title-color">${taskData.title}</h5>
+<p class="card-text">${taskData.description}</p>
+<a href="#" class="btn btn-primary">${taskData.type}</a>
+</div>
+</div>
+  </div>`;
+};
